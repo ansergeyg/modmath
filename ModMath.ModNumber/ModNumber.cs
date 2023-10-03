64dc70c55f925639c387d86ed2d406f80e513283
Sergey An <ansergeyg@gmail.com>
2023-10-03 22:18:08 +0200

    Implement the division operation under modulus

--- ModMath.ModNumber/ModNumber.cs
+++ ModMath.ModNumber/ModNumber.cs
@@ -21,11 +21,35 @@ public struct ModNumber
         new ModNumber(left._modValue * right._modValue % s_modulus);
 
     public static ModNumber operator /(ModNumber left, ModNumber right) =>
-        new ModNumber(left._modValue / right._modValue);
+        new ModNumber(left._modValue * GetModInverse(right._modValue) % s_modulus);
 
     public static implicit operator int(ModNumber modNumber) => modNumber._modValue;
 
     public override string ToString() => $"{_modValue}";
+
+    private static ModNumber BinaryPow(int baseNumber, int power)
+    {
+        ModNumber modResult = new ModNumber(1);
+        ModNumber modBase = new ModNumber(baseNumber);
+
+        while (power > 0)
+        {
+            if (power % 2 == 1)
+            {
+                modResult *= modBase;
+            }
+
+            modBase *= modBase;
+            power >>= 1;
+        }
+
+        return modResult;
+    }
+
+    private static ModNumber GetModInverse(int number)
+    {
+        return BinaryPow(number, s_modulus - 2);
+    }
 
     public static void SetModulus(int modulus)
     {
--- ModMath.Tests/ModNumberTest.cs
+++ ModMath.Tests/ModNumberTest.cs
@@ -5,11 +5,30 @@ using ModNumber;
 public class ModNumberTest
 {
     [Fact]
-    public void Test_ToString_Conversion()
+    public void Test_implicit_int_conversion()
     {
         ModNumber.SetModulus(7);
         ModNumber modNum = new ModNumber(8);
 
         Assert.Equal(1, modNum);
+    }
+
+    [Fact]
+    public void Test_to_string_conversion()
+    {
+        ModNumber.SetModulus(7);
+        ModNumber modNum = new ModNumber(8);
+
+        Assert.Equal("1", modNum.ToString());
+    }
+
+    [Fact]
+    public void Test_divison_with_modulus()
+    {
+        ModNumber.SetModulus(7);
+        ModNumber a = new ModNumber(9);
+        ModNumber b = new ModNumber(3);
+
+        Assert.Equal(3, a / b);
     }
 }
